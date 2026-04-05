5bf0555f6fda6e83a74952a32cdbc17a01b6af41
tlima1011 <thiagolima1011@gmail.com>
2026-04-04 21:54:19 -0300

    Projeto Criado

---
+++ src/com/company/app/Program.java
@@ -0,0 +1,11 @@
+package com.company.app;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Projeto Criado Java");
+		
+	}
+
+}
---
+++ src/com/company/entities/Worker.java
@@ -0,0 +1,7 @@
+package com.company.entities;
+
+public class Worker {
+	
+	private String name; 
+	
+}
