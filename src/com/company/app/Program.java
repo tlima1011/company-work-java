ed03eb03359ad98239aae11d31d7fb9be85a6389
tlima1011 <thiagolima1011@gmail.com>
2026-04-04 22:19:13 -0300

    Classe Worker Criada com testes de associação

--- src/com/company/app/Program.java
+++ src/com/company/app/Program.java
@@ -1,11 +1,41 @@
 package com.company.app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import com.company.entities.Department;
+import com.company.entities.Worker;
+import com.company.entities.enums.WorkLevel;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Projeto Criado Java");
+		Locale.setDefault(Locale.US); 
+		Scanner ler = new Scanner(System.in); 
 		
+		System.out.print("Enter department's name: ");
+		String dept = ler.nextLine(); 
+		
+		Department department = new Department(dept); 
+		
+		System.out.print("Name: ");
+		String Name = ler.nextLine(); 
+		//Name: Alex
+		System.out.print("Level (Junior/MidLevel/Senior): ");
+		String Level = ler.nextLine(); 
+		//MidLevel
+		WorkLevel workLevel = WorkLevel.valueOf(Level);
+		System.out.print("Base salary: ");
+		Double baseSalary = ler.nextDouble(); 
+		Worker wk = new Worker(Name, workLevel, baseSalary, department);
+		//String n, WorkLevel l, Double bs, Department d
+		//1200.00
+		System.out.println(wk);
+		
+		
+		
+		ler.close();
 	}
 
 }
--- src/com/company/entities/Worker.java
+++ src/com/company/entities/Worker.java
@@ -1,7 +1,28 @@
 package com.company.entities;
 
+import com.company.entities.enums.WorkLevel;
+
 public class Worker {
 	
 	private String name; 
+	private WorkLevel level; 
+	private Double baseSalary; 
+	private Department department; 
+	
+	public Worker(String n, WorkLevel l, Double bs, Department d) {
+		this.name = n; 
+		this.level = l; 
+		baseSalary = bs; 
+		department = d; 
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [name=" + name + ", level=" + level + ", baseSalary=" + baseSalary + ", department=" + department
+				+ "]";
+	}
+	
+	
+	
 	
 }
